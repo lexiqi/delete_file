184ba5315714a371d8cea642db06038b143bd12c
qxt <644360894@qq.com>
2019-01-20 16:59:51 +0800

    第一次提交

---
+++ rmfile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/qxt/yolo3_cj
+filenum=$(ls *.h5 -l|grep "^-"|wc -l)
+index=0
+if [ $filenum -gt 2 ]
+then
+	ls -rt *.h5 > ls.log
+	for i in $(cat ls.log)
+	do
+		index=$(($index+1))
+		delnum=$(($filenum-2))
+		if [ $index -le $delnum ]
+		then
+			rm -rf /home/qxt/yolo3_cj/$i
+		fi
+	done
+	rm -rf /home/qxt/yolo3_cj/ls.log 
+fi
